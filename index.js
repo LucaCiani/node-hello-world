8c9d9b51719ccbb87e28fa3eb35b1726d68b9b26
LucaCiani <lucaciani88@gmail.com>
2025-06-26 16:39:04 +0200

    all bonus complete

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+import axios from "axios";
+
+import chalk from "chalk";
+
+import "dotenv/config";
+console.log(process.env.SECRET_KEY);
+
 const text = process.argv[2];
-console.log(`Hello ${text}`);
+console.log(chalk.blue(`Hello ${text}`));
+
+axios({
+    method: "get",
+    url: "https://lanciweb.github.io/demo/api/pictures/",
+}).then(function (response) {
+    console.log(response.data);
+});
